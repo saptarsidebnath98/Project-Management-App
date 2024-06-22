6c893e375608b2b65472141b2c5b243271f4f046
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-22 14:13:46 +0530

    componenets commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="">
+      <ProjectSidebar/>
+    </main>
   );
 }
 
---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,14 @@
+export default function ProjectSidebar() {
+  return (
+      <aside>
+          <h2>Your Projects</h2>
+          <div>
+              <button>
+                 + Add Project
+              </button>
+          </div>
+          <ul></ul>
+      </aside>
+  
+  )
+}
