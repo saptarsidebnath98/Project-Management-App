5a0fddd408301ff4dc998cee848fb4399088c4ea
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-23 10:42:36 +0530

    NoProjectSelected.jsx created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar/>
-      <NewProject/>
+      {/* <NewProject/> */}
+      <NoProjectSelected/>
     </main>
   );
 }
