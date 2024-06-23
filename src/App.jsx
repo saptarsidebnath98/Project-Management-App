4c8c77af319909fc2e1370f47d800c6ec3d63f24
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-23 10:22:21 +0530

    designed jsx file Input,NewProject with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
 
 function App() {
   return (
-    <main className="">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar/>
+      <NewProject/>
     </main>
   );
 }
